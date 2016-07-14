161ed17fcd052f3419bf827af6a96214a4640233
XPD Operator <xf28id1@xf28id1-ws2.cs.nsls2.local>
2016-07-14 19:29:59 -0400

    update script

--- get_metadata.py
+++ get_metadata.py
@@ -11,6 +11,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 h = db[-5:]
 evs = get_events(h)
@@ -18,27 +19,21 @@ field = ['time', 'gas', 'temperature','rga_mass1', 'rga_mass2','rga_mass3']
 col_num = len(field)
 full_list = []
 for ev in evs:
-    row_list = []
     for el in field:
         try:
-            row_list.append(ev['data'].get(el))
+            full_list.append(ev['data'].get(el))
         except:
-            row_list.append(None)
-    full_list.append(rga_mass_list)
+            full_list.append('N/A')
 md_array = np.reshape(full_list, (len(full_list)/col_num, col_num))
-md_df = pd.DataFrame(md_array, field)
+md_df = pd.DataFrame(md_array, columns=field)
 md_df.to_csv('med_md.csv', index=False)
 
 
-#DataOut = c_[Time,Temperature]
-#np.savetxt("/home/xf28id1/xpdUser/tiff_base/T_profile.DAT", DataOut)
-
-
-# Plot the T vs Time
-#D = np.loadtxt("/home/xf28id1/xpdUser/tiff_base/T_profile.DAT")
-#plot(D[:,0],D[:,1],'o-')
-#xlabel('Time')
-#ylabel('Temperature')
-#show()
-
-
+# Plot temperature vs rga_mass1
+"""
+fig = plt.figure()
+plt.plot(md_array[2,:],D[:,3],'o-')
+xlabel('temperature')
+ylabel('rga_mass1')
+plt.show()
+"""
