b19521ef3ddeef6c46c56f18aad277eff039d36d
Timothy Liu <chbrahmsliu@gmail.com>
2016-08-02 12:38:45 -0400

    update dark_sub

--- dark_sub.py
+++ dark_sub.py
@@ -2,7 +2,10 @@ import os
 import tifffile as tif
 
 def dark_sub(dark_img_f, data_dir=os.getcwd()):
-    un_sub_list = [f for f in os.listdir() if not f.startswith('sub')]
+    un_sub_list = [f for f in os.listdir() if not f.startswith('sub')
+                   and f.endswith('.tif')]
+    if dark_img_f in un_sub_list:
+        un_sub_list -= dark_img_f
     dark_img = tif.imread(dark_img_f)
     for el in un_sub_list:
         sub_name = 'sub_'+el
